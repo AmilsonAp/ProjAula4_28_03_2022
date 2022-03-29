163ef2690522eb5ec532d0cc65b3942cdbbd66a7
AmilsonAp <amilson.moreira@gmail.com>
2022-03-28 21:58:38 -0300

    V0

---
+++ ProjAula4/Carro.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjAula4
+{
+    public class Carro
+    {
+        #region Propriedades
+        public int Id { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int AnoModelo { get; set; }
+
+        public int AnoFabricacao { get; set; }
+
+        public string Cor { get; set; }
+
+        public int Velocidade { get; set; }
+
+        public bool StatusCarro { get; set; }
+
+        public bool Buzinar { get; set; }
+
+        public bool Ar { get; set; }
+        #endregion
+
+        #region Metodos
+        //Acelera
+        public void Acelerar(int Velocidade)
+        {
+            this.Velocidade += Velocidade;
+        }
+
+        //Freia
+        public void Freiar(int Velocidade)
+        {
+            this.Velocidade -= Velocidade;
+        }
+
+        //Liga
+        public void Ligar()
+        {
+            this.StatusCarro = true;
+        }
+
+        //Desliga
+        public void Desligar()
+        {
+            this.StatusCarro = false;
+        }
+
+        //rAr
+        public void LigarAr()
+        {
+            if (this.Ar == false)
+            {
+                if (this.StatusCarro == true)
+                {
+                    this.Ar = true;
+                }
+                else
+                    Console.WriteLine("O carro esta desligado!");
+            }
+            else
+                Console.WriteLine("O ar já esta ligado!");
+        }
+        public void DesligarAr()
+        {
+            if (this.Ar == true)
+            {
+                this.Ar = false;
+            }
+            else
+                Console.WriteLine("O ar condicionado já esta desligado");
+        }
+
+        //Buzina
+        public void LigarBuzina()
+        {
+            this.Buzinar = true;
+        }
+
+        public void DesligarBuzina()
+        {
+            this.Buzinar = false;
+        }
+      
+
+        public override string ToString()
+        {
+            return "\nId: " + this.Id +
+                "\nMarca: " + this.Marca +
+                "\nModelo: " + this.Modelo +
+                "\nAno Modelo: " + this.AnoModelo +
+                "\nAno Fabricação: " + this.AnoFabricacao +
+                "\nCor: " + this.Cor +
+                "\nStaus Carro: " + this.StatusCarro +
+                "\nVelocidade: " + this.Velocidade +
+                "\nBuzina: " + this.Buzinar +
+                "\nAr: " + this.Ar;
+
+        }
+        #endregion
+
+    }
+}
---
+++ ProjAula4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using ProjAula4;
+
+Carro carro1 = new Carro();
+
+carro1.Id = 1;
+carro1.Marca = "Fiat";
+carro1.Modelo = "Toro";
+carro1.AnoFabricacao = 2022;
+carro1.AnoModelo = 2022;
+carro1.Cor = "Vermelho";
+carro1.Ligar();
+carro1.Acelerar(150);
+carro1.LigarBuzina();
+carro1.LigarAr();
+
+Carro carro2 = new Carro()
+{
+    Id = 2,
+    Marca = "Hyundai",
+    Modelo = "HB20",
+    AnoFabricacao = 2020,
+    AnoModelo = 2021,
+    Cor = "Preto"
+};
+
+Console.WriteLine(carro1);
+Console.WriteLine(carro2);
